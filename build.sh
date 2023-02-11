a92e4618329835211af8e56e1a3c83b722c945e0
Gabriele Gigante <gabriele@pickeat.it>
2023-02-11 19:12:07 +0100

    script update

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 mkdir -p tmp
-GOOS=linux GOARCH=arm64 go build -o tmp/bootstrap main.go
-zip -j tmp/bootstrap.zip tmp/bootstrap
+GOOS=linux GOARCH=arm64 go build -o tmp/bootstrap 
+./generate-docs.sh
+cd tmp && zip -j bootstrap.zip bootstrap && cd ..
